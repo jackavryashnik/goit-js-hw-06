6c29b2d687e9baddd1cdd32e4739d64ced8ada5e
jackavryashnik <jackavryashnik@gmail.com>
2023-11-13 22:05:51 +0200

    first commit

---
+++ js/task-2.js
@@ -0,0 +1,25 @@
+class Storage {
+  #items;
+  constructor(arr) {
+    this.#items = arr;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(e => e !== itemToRemove);
+  }
+}
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
